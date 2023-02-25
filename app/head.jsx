c546b87f4730d4babd236bcc9d3cc38ce63db428
Walter Celiz <wceliz.dev@gmail.com>
2023-02-25 14:57:45 -0300

    fix: first content package.json dependencies

--- app/head.jsx
+++ app/head.jsx
@@ -3,7 +3,7 @@ function Head() {
     <>
       <title>Metaversus</title>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
-      {/* <link rel="icon" href="/favicon.ico" /> */}
+      <link rel="icon" href="/favicon.ico" />
       <link rel="preconnect" href="https://stijndv.com" />
       <link
         rel="stylesheet"
